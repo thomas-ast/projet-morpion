6482e2a6c53247f215fc52580d7517d86ae3c557
Thomas Astrand <thomas.astrand@alumni.univ-avignon.fr>
2020-05-13 18:09:14 +0200

    Very first gui

--- src/application/Launcher.java
+++ src/application/Launcher.java
@@ -1,19 +1,28 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
-public class Main extends Application {
+/**
+ * Launches the application
+ * @author thomas
+ */
+public class Launcher extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("views/gui.fxml"));;
+			Scene scene = new Scene(root,1280,720);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setMinWidth(800);
+			primaryStage.setMinHeight(600);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
