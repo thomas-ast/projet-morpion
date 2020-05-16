8f8697bb9e5738da9bcb04e3560039361d9f7404
Volokyn <volokyn@gmail.com>
2020-05-16 14:42:15 +0200

    Added reset and isFull functions, and javadoc

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,13 +1,40 @@
 package application;
 import java.util.Random;
 
+/**
+ * This class represent the game.
+ * 
+ * @author Lucas DAMIENS
+ *
+ */
 public class Game {
 	
+	/**
+	 * Array representing the tic tac toe game.
+	 */
 	private char grid[][];
+	
+	/**
+	 * Boolean used to check if game is over.
+	 */
 	private boolean is_over;
+	
+	/**
+	 * Int used to know which types of game it is.
+	 * 1=player vs player
+	 * 2=player vs computer
+	 */
 	private int type;
+	
+	/**
+	 * Char used to know which players turn it is.
+	 * 'X' or 'O'
+	 */
 	private char current_player;
 	
+	/**
+	 * Constructor
+	 */
 	public Game(int _type) {
 		this.grid= new char[3][3];
 		this.setIs_over(false);
@@ -15,6 +42,9 @@ public class Game {
 		
 	}
 	
+	/**
+	 * Function used to check if one of the player as won.
+	 */
 	public void checkWin() {
 		for(int i = 0; i<3; i++)
 		{
@@ -43,14 +73,28 @@ public class Game {
 		}
 	}
 
+	/**
+	 * getter for the boolean is_over
+	 * @return {@code boolean}
+	 */
 	public boolean getIs_over() {
 		return is_over;
 	}
 
+	/**
+	 * setter for the boolean is_over
+	 * @param is_over
+	 */
 	public void setIs_over(boolean is_over) {
 		this.is_over = is_over;
 	}
 	
+	
+	/**
+	 * Used for the beginning of the game.
+	 * Random number choosing which player 
+	 * will play first.
+	 */
 	public void setCurrent_player()
 	{
 		Random randomNum = new Random();
@@ -63,6 +107,9 @@ public class Game {
 		
 	}
 	
+	/**
+	 * Function used to switch turns.
+	 */
 	public void switchPlayer() {
 		
 		if(current_player=='X')
@@ -71,7 +118,44 @@ public class Game {
 			current_player='X';
 			
 	}
+	
+	/**
+	 * Function used to reset the game.
+	 */
+	public void reset() {
+		for(int i=0; i<3; i++)
+		{
+			for(int j =0; j<3; j++)
+			{
+				this.grid[i][j]=' ';
+			}
+		}
+	}
+
+	/**
+	 * Function used to check if the grid is full.
+	 * @return {@code boolean}
+	 */
+	public boolean isFull() {
+		boolean full=false;
 		
+		for(int i=0; i<3; i++)
+		{
+			for(int j =0; j<3; j++)
+			{
+				if(this.grid[i][j]=='X' || this.grid[i][j]=='O' || full!=true)
+				{
+					full=true;
+				}
+				else
+				{
+					full=false;
+				}
+			}
+		}
+		
+		return full;
+	}
 }
 
 
