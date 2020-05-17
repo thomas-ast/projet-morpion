f0593fd640d3685fc22e1ea6d595b30f09d9aa12
Thomas Astrand <thomas.astrand@alumni.univ-avignon.fr>
2020-05-17 15:21:28 +0200

    Naming refactor, delete crap

--- src/application/MenuController.java
+++ src/application/MenuController.java
@@ -5,7 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-public class Controller {
+public class MenuController {
 	
 	/**
 	 * Button to play vs the AI
